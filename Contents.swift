00486d15987ac9cb9167741e67dafa09a0efcdfa
Dngn20 <dngu9879@740s-pl-998.local>
2016-09-15 13:13:31 -0600

    String playing it was practice yay

---
+++ Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let multipier = 2
+let message = (Double)(multipier) * 2
+
+
+var String1 = "I want "
+var String2 = "to go home"
+var String3 = " and sleep at my bed"
+var story = String1 + String2 + String3
+print("The story is \(story)")
+
+
+let sentance = "This is a sentance."
+let sentance2 = "This is a gooder sentance"
+if sentance == sentance2 {
+    print("These sentances are equal")
+}
+else {
+    print("These sentances are NOT equal")
+}
+
+var word = "wordas"
+print(word)
+word = "wordan"
+print(word)
+word =  "I'm tried"
+print(word)
